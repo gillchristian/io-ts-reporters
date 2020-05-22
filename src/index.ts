a55b017effcd0c710508902e632441927411c4df
Omar Diab <me@omardiab.com>
2020-05-22 19:54:46 +0900

    support for error messages (#25)
    * add io-ts-types dev dep
    
    * Yarn.lock
    
    * failing test for brand message
    
    * add printing error messages
    
    * tslint fix
    
    * no period after message in tests
    
    * remove message: prefix and simplify template string
    
    * fix test

--- src/index.ts
+++ src/index.ts
@@ -20,16 +20,20 @@ export const formatValidationError = (error: t.ValidationError) => {
         error.context as Array<t.ContextEntry>
     );
 
-    return pipe(maybeErrorContext, map(errorContext => {
-        const expectedType = errorContext.type.name;
-        return (
+    return pipe(
+        maybeErrorContext,
+        map((errorContext) => {
+            const expectedType = errorContext.type.name;
             // https://github.com/elm-lang/core/blob/18c9e84e975ed22649888bfad15d1efdb0128ab2/src/Native/Json.js#L199
-            // tslint:disable-next-line:prefer-template
-            `Expecting ${expectedType}`
-                + (path === '' ? '' : ` at ${path}`)
-                + ` but instead got: ${jsToString(error.value)}.`
-        );
-    }))
+            return [
+                `Expecting ${expectedType}`,
+                path ? `at ${path}` : '',
+                'but instead got:',
+                jsToString(error.value),
+                error.message ? `(${error.message})` : ''
+            ].filter(Boolean).join(' ')
+        })
+    );
 };
 
 export const reporter = <T>(validation: t.Validation<T>) => (
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,5 @@
 import * as iots from 'io-ts';
+import { withMessage } from 'io-ts-types/lib/withMessage';
 import * as test from 'tape';
 
 import { reporter } from '../src/';
@@ -15,7 +16,7 @@ test('formats a top-level primitve type correctly', (t) => {
     const PrimitiveType = iots.string;
     const result = PrimitiveType.decode(42);
 
-    t.deepEqual(reporter(result), ['Expecting string but instead got: 42.']);
+    t.deepEqual(reporter(result), ['Expecting string but instead got: 42']);
     t.end();
 });
 
@@ -23,7 +24,7 @@ test('formats array items', (t) => {
     const NumberGroups = iots.array(iots.array(iots.number));
     const result = NumberGroups.decode({});
 
-    t.deepEqual(reporter(result), ['Expecting Array<Array<number>> but instead got: {}.']);
+    t.deepEqual(reporter(result), ['Expecting Array<Array<number>> but instead got: {}']);
     t.end();
 });
 
@@ -31,7 +32,7 @@ test('formats nested array item mismatches correctly', (t) => {
     const NumberGroups = iots.array(iots.array(iots.number));
     const result = NumberGroups.decode([[{}]]);
 
-    t.deepEqual(reporter(result), ['Expecting number at 0.0 but instead got: {}.']);
+    t.deepEqual(reporter(result), ['Expecting number at 0.0 but instead got: {}']);
     t.end();
 });
 
@@ -53,11 +54,38 @@ test('formats a complex type correctly', (t) => {
     });
 
     t.deepEqual(reporter(result), [
-        'Expecting number at age but instead got: undefined.',
-        'Expecting "Male" at gender.0 but instead got: undefined.',
-        'Expecting "Female" at gender.1 but instead got: undefined.',
-        'Expecting "Male" at children.0.gender.0 but instead got: "Whatever".',
-        'Expecting "Female" at children.0.gender.1 but instead got: "Whatever".'
+        'Expecting number at age but instead got: undefined',
+        'Expecting "Male" at gender.0 but instead got: undefined',
+        'Expecting "Female" at gender.1 but instead got: undefined',
+        'Expecting "Male" at children.0.gender.0 but instead got: "Whatever"',
+        'Expecting "Female" at children.0.gender.1 but instead got: "Whatever"'
     ]);
+    t.end();
+});
+
+test('formats branded types correctly', (t) => {
+    interface PositiveBrand {
+        readonly Positive: unique symbol;
+    }
+
+    const Positive = iots.brand(
+        iots.number,
+        (n): n is iots.Branded<number, PositiveBrand> => n >= 0,
+        'Positive'
+    );
+
+    t.deepEqual(reporter(Positive.decode(-1)), [
+        'Expecting Positive but instead got: -1'
+    ]);
+
+    const PatronizingPositive = withMessage(
+        Positive,
+        (_i) => `Don't be so negative!`
+    );
+
+    t.deepEqual(reporter(PatronizingPositive.decode(-1)), [
+        'Expecting Positive but instead got: -1 (Don\'t be so negative!)'
+    ]);
+
     t.end();
 });
